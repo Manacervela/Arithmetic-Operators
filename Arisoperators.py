182c78f3033dfeae19f3713b7cc77d8580f367c9
Manacervela <120068817+Manacervela@users.noreply.github.com>
2022-12-08 17:03:58 +1300

    Update Arisoperators.py

--- Arisoperators.py
+++ Arisoperators.py
@@ -1,1 +1,7 @@
-print("Hello")
+# Arithmetic Operators
+
+a = float(input("a -> "))
+b = float(input("b -> "))
+
+operators = ((3+3*8)<3 and (-6/3*4)+2<2) or (a>b)
+print(f"is equal to: {operators}")
